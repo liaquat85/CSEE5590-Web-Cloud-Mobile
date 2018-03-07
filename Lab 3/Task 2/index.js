68c3bdc3dfcf718fadce42204f759d5d24914d98
Ali <la5w7@umkc.edu>
2018-03-07 12:58:25 -0600

    Lab 3 task 2

--- Lab 3/Task 2/index.js
+++ Lab 3/Task 2/index.js
@@ -48,6 +48,7 @@ app.use(express.static('public'));
 app.post('/twitter/user', function (req, res) {
     var username = req.body.username;
     var twitJSON = {name: username, children: []};
+// to get friend list of entered user from twitter server
 T.get('friends/list', {screen_name: username}, function (err, data, response) {
     if (err) throw err;
     for (var i = 0; i < data['users'].length; i++) {
@@ -58,10 +59,11 @@ T.get('friends/list', {screen_name: username}, function (err, data, response) {
     fs.writeFile("C:\\Users\\la5w7\\Desktop\\Spring 2018\\CSEE5590-Web-Cloud-Mobile\\Lab 3\\Task 2\\public\\input.json", JSON.stringify(twitJSON), function (err) {
         if (err) throw err;
         console.log('Twitter users written to file.');
+        // write to console to see whether the JSON file is created with friendlist 
     });
 });
 
-console.log(twitJSON);
+//console.log(twitJSON);
 
     var data = twitter.getUser({screen_name: username}, function (error, response, body) {
         res.status(404).send({
@@ -72,41 +74,10 @@ console.log(twitJSON);
             result: {
                 "userData": data
             }
+            // returns the data of the entered user to html file
         });
     });
 });
-
-var Twit = require('twit');
-var fs = require('fs');
-
-var T = new Twit({
-
-    consumer_key: 'RFgBoK4ZioKHlDVesZ2EbBjJp',
-    consumer_secret: '7wnwY51LrRS6AtyJMGrDi7yXpSNEHMcNLcjHspieFlwQztMBjz',
-    access_token: '477630074-nOdC9Nb7GAh8VgdOEbkT44Z9geWQyqxZncI0DcaF',
-    access_token_secret: 'Pb7PLjd7jyyW2P0e8IMO4xHe0QwBDRTUvhqSZugR1Rl3Q'
-
-});
-
-//
-//  get the list of user id's that follow @3guysinagarage
-//
-var twitJSON = {name: "@username", children: []};
-//
-// T.get('friends/list', {screen_name: 'liaquat85'}, function (err, data, response) {
-//     if (err) throw err;
-//     for (var i = 0; i < data['users'].length; i++) {
-//         var obj = {name: "@" + data['users'][i]['screen_name'], children: []};
-//         console.log(obj);
-//         twitJSON.children.push(obj);
-//     }
-//     fs.writeFile("input.json", JSON.stringify(twitJSON), function (err) {
-//         if (err) throw err;
-//         console.log('Twitter users written to file.');
-//     });
-// });
-//
-// console.log(twitJSON);
 
 
 var server = app.listen(3000, function () {
